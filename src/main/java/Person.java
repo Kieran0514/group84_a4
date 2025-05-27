6a267769d3ef163cc88794dd36d60c90e7a618d1
Kieran0514 <S4090514@student.rmit.edu.au>
2025-05-27 21:55:06 +1000

    added all checks for addPerson() (not tested, still need to add txt file modification)

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,6 +1,8 @@
 import java.util.HashMap;
 import java.util.Date;
 import java.util.Scanner;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 
@@ -16,20 +18,98 @@ public class Person {
     private boolean isSuspended;
 
     public boolean addPerson(String id, String fname, String lname, String addy, String bdate) {
-    // Condition 1: personID should be exactly 10 characters long;
-    // the first two characters should be numbers between 2 and 9,
-    // there should be at least two special characters between characters 3 and 8,
-    // and the last two characters should be uppercase letters (A-Z). Example: "56s_d%&fAB"
+        // Condition 1: personID should be exactly 10 characters long;
+        // the first two characters should be numbers between 2 and 9,
+        // there should be at least two special characters between characters 3 and 8,
+        // and the last two characters should be uppercase letters (A-Z). Example: "56s_d%&fAB"
 
-    // Condition 2: The address of the Person should follow the following format:
-    // Street Number|Street|City|State|Country.
-    // The State should be only Victoria.
-    // Example: 32|Highland Street|Melbourne|Victoria|Australia.
+        // Condition 2: The address of the Person should follow the following format:
+        // Street Number|Street|City|State|Country.
+        // The State should be only Victoria.
+        // Example: 32|Highland Street|Melbourne|Victoria|Australia.
 
-    // Condition 3: The format of the birthdate of the person should follow the following format: DD-MM-YYYY. 
-    // Example: 15-11-1990
+        // Condition 3: The format of the birthdate of the person should follow the following format: DD-MM-YYYY. 
+        // Example: 15-11-1990
 
-        return true;
+        boolean status = false;
+        int i;
+        int counter= 0;
+
+        //check id is valid
+        if (id.length() == 10) { //check for length of 10, if not return false
+            for (i = 0; i < 2; ++i) { //iterate thru first 2 letters of the id
+                if (Character.isDigit(id.charAt(i))) { //check that the characters are digits, else break and return false
+                    if (Character.getNumericValue(id.charAt(i)) >= 2 && Character.getNumericValue(id.charAt(i)) <= 9) { //check that they're between 2 and 9, else break and return false
+                        for (i = 2; i < 8; ++i) { //iterate thru characters 3 and 8
+                            if (!Character.isLetterOrDigit(id.charAt(i)) && id.charAt(i) != ' ') { //check if characters are special characters
+                                ++counter; //if so, add to the counter
+                            }
+                        }
+
+                        if (counter >= 2) { //if special character counter is equal or above 2
+                            for (i = 8; i < 10; ++i) { //iterate thru the last 2 characters
+                                if (Character.isUpperCase(id.charAt(i))) { //if they're uppercase letters, else return false and break
+                                    status = true; //return true
+                                }
+                                else {
+                                    status = false;
+                                    break;
+                                }
+                            }
+                        }
+                        else {
+                            status = false;
+                        }
+                    }
+                    else {
+                        status = false;
+                        break;
+                    }
+                }
+                else {
+                    status = false;
+                    break;
+                }
+            }
+        }
+        else {
+            status = false;
+        }
+
+        //check address is valid and formatted correctly
+        String[] addressParts = addy.split("\\|"); //split the address using | as a delimiter
+        if (addressParts.length == 5) { //check there are exactly the 5 required components of the address, else return false
+            if(addressParts[3] == "Victoria") { //ensure the state is victoria, else return false
+                status = true;
+            }
+            else {
+                status = false;
+            }
+        }
+        else {
+            status = false;
+        }
+
+        //check date is formatted correctly
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy"); //use dateTimeFormatter to determine required date pattern
+        try {
+            LocalDate.parse(bdate, dateFormat); //try parse bdate as date in the correct format, if success return true
+            status = true;
+        } catch (DateTimeParseException e) { //else return false
+            status = false;
+        }
+
+        if (!status) { //let the user know the requirements for adding a person if it fails
+            System.out.println("Failed to add person. Please ensure the following: ");
+            System.out.println("- ID has exactly 10 characters");
+            System.out.println("- First 2 characters of ID must be a number between 2 and 9");
+            System.out.println("- ID should have at least 2 special characters between characters 3 and 8");
+            System.out.println("- Last 2 characters of ID must be uppercase letters");
+            System.out.println("- Address should formatted as such: Street Number|Street|City|State|Country");
+            System.out.println("- Birthdate should be formatted as such: DD-MM-YYYY");
+        }
+
+        return status;
     }
 
     public boolean updatePersonalDetails(String id, String fname, String lname, String addy, String bdate) {
